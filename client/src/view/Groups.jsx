8ad8fc96120eadf0c9b353ed28a41344cfe376fb
Anjali Prajapti <anjaliprajapati3122@gmail.com>
2019-03-29 20:59:21 +0530

    connect groups view with redux store

--- client/src/view/Groups.jsx
+++ client/src/view/Groups.jsx
@@ -1,25 +1,53 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types';
 import withStyles from "@material-ui/core/styles/withStyles";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import List from "@material-ui/core/List";
-import Collapse from "@material-ui/core/Collapse";
-import Button from "../components/CustomButtons/Button.jsx"
-import buttonStyle from "../assets/jss/material-dashboard-pro-react/components/buttonStyle.jsx";
-import cx from "classnames";
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { Grid } from '@material-ui/core';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { fetchGroups } from '../actions/groups.action';
 
+const styles = theme => ({
+    button: {
+      margin: theme.spacing.unit,
+    },
+    input: {
+      display: 'none',
+    },
+    progress: {
+        margin: theme.spacing.unit * 2,
+    },
+});
 class Groups extends Component {
+
+    componentDidMount() {
+        this.props.fetchGroups("1", this.props.history);
+    }
 
     render() {
 
-        console.log("inside subject b4 the code---------------");
+        const {
+            classes,
+            groupsDetails,
+            groupsLoading,
+            groupsError
+        } = this.props;
 
+        if(groupsLoading &&  !groupsDetails.length) {
+            return(
+                <CircularProgress className={classes.progress} />
+            )
+        } 
+
+        if(groupsError && !groupsDetails.length) {
+            return(
+                <div><h6>Hard luck</h6></div>
+            )
+        }
 
         return (
             <div >
-                <h1>SUBJECTS</h1>
+                <h1></h1>
             </div>
         )
     }
@@ -27,4 +55,18 @@ class Groups extends Component {
 
 
 
-export default Groups;
+const mapStateToProps = (state) => {
+    return {
+        groupsDetails: state.groups.groupsDetails,
+        groupsLoading: state.groups.groupsLoading,
+        groupsError: state.groups.groupsError
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchGroups: (course_id, history) => dispatch(fetchGroups(course_id, history)),
+    }
+}
+
+export default compose(withStyles(styles), connect(mapStateToProps, mapDispatchToProps)) (Groups);
