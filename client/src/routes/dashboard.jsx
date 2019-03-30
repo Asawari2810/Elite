45bdf4b5d0e99afdbb41e8d1993dbd27e5434988
unknown <asawari.tare@gmail.com>
2019-03-30 16:16:08 +0530

    Added new route FullTestLink to Dashboard.

--- client/src/routes/dashboard.jsx
+++ client/src/routes/dashboard.jsx
@@ -3,6 +3,8 @@ import dashboard from "../view/UserDashboard.jsx";
 import Subjects from "../view/Subjects.jsx";
 import Groups from '../view/Groups.jsx';
 import ModalPaperList from '../view/ModalPaperList';
+import FullTestList from '../view/FullTestList';
+
 
 const demoRoutes = [];
 export const dashboardRoutes = [
@@ -22,6 +24,12 @@ export const otherRoutes = [
         path: '/modalList/:id', 
         component : ModalPaperList, 
         name: 'Modal Paper List'
+    },
+
+    {
+        path: '/fullTestList',
+        component : FullTestList, 
+        name: 'Full Test List'
     }
 
 ]
