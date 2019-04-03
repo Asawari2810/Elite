e92007352191c3aa72a9e21394148e0fb5c11289
Anjali Prajapti <anjaliprajapati3122@gmail.com>
2019-04-03 15:18:03 +0530

    api variable for GET_COURSES in config fil

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -2,5 +2,6 @@ export const API_ROOT = 'http://5c9e1aba595c55001487be74.mockapi.io/';
 
 export const URI = {
     GROUPS: 'groups',
-    MODAL_LIST: 'modalList'
+    MODAL_LIST: 'modalList',
+    GET_COURSES: '',
 }
