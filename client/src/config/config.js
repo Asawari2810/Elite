3846318aad285f85c4c952accb7517d0c15c6a1f
Anjali Prajapti <anjaliprajapati3122@gmail.com>
2019-04-06 03:16:08 +0530

    api config for groups apis

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,9 +1,8 @@
 export const API_ROOT = 'http://localhost:5000/api/';
 
 export const URI = {
-    GROUPS: 'groups',
     MODAL_LIST: 'modalList',
     GET_COURSES: 'course',
     ADD_COURSE: 'course',
-    GET_GROUPS: 'group',
+    GROUPS: 'group',
 }
