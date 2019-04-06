ac8d277641011a546336fc711f495018985f3ea4
unknown <asawari.tare@gmail.com>
2019-04-06 13:23:28 +0530

    Added entries for 2 API's. FullTest and FullTestSubmitScore

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -2,5 +2,7 @@ export const API_ROOT = 'http://5c9e1aba595c55001487be74.mockapi.io/';
 
 export const URI = {
     GROUPS: 'groups',
-    MODAL_LIST: 'modalList'
+    MODAL_LIST: 'modalList',
+    FULL_TEST_QUESTIONS : 'fullTestQuestions',
+    FULL_TEST_SUBMIT_SCORE : 'FullTestSubmitScore'
 }
